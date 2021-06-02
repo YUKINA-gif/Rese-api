191d7c6dda18319147b77896d7979022de68070b
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-06-02 12:56:43 +0900

    storesseeder修正

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -105,16 +105,6 @@ class StoreSeeder extends Seeder
             $param);
 
         $param = [
-            "area_id" => 3,
-            "genre_id" => 3,
-            "name" => "戦慄",
-            "overview" => "気軽に立ち寄れる昔懐かしの大衆居酒屋です。キンキンに冷えたビールを、なんと199円で。鳥かわ煮込み串は販売総数100000本突破の名物料理です。仕事帰りに是非御来店ください。",
-            "image" => "https://coachtech-matter.s3-ap-northeast-1.amazonaws.com/image/izakaya.jpg",
-        ];
-        DB::table("stores")->insert(
-            $param);
-
-        $param = [
             "area_id" => 1,
             "genre_id" => 1,
             "name" => "築地色合",
